cc98a5edfb783327c102e399d3162ab0418ab0c4
Yuliya BERT <98463848+ElkaXX@users.noreply.github.com>
2024-01-02 11:09:15 +0100

    First commit

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,7 +10,15 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const modalForm = document.querySelector(".modal-form");
+const modalCloseBtn = document.querySelector(".close");
+const modalFinish = document.querySelector(".modal-finish");
+
+// Form elements
+const firstName = document.querySelector("#first");
+const lastName = document.querySelector("#last");
+const email = document.querySelector("#email");
+const birthdate = document.querySelector("#birthdate");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +27,3 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
-
-
