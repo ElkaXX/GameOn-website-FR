89ab9acb594c135a49553cf3d729fedd80b091fc
Yuliya BERT <98463848+ElkaXX@users.noreply.github.com>
2024-01-04 10:46:38 +0100

    Correction HTML

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,12 +13,16 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const modalForm = document.querySelector(".modal-form");
 const modalCloseBtn = document.querySelector(".close");
 const modalFinish = document.querySelector(".modal-finish");
+const modalFinishBtn = document.querySelector(".modal-finish-btn");
 
 // Form elements
 const firstName = document.querySelector("#first");
 const lastName = document.querySelector("#last");
 const email = document.querySelector("#email");
 const birthdate = document.querySelector("#birthdate");
+const quantity = document.querySelector("#quantity");
+const locations = document.querySelectorAll("input[name='location']");
+const checkbox = document.querySelector("#checkbox1");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
